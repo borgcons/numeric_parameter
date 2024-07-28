994537ad17d4b38b06cd6348cc9ecc41b5c634d7
Michael Borghardt <30298366+borgcons@users.noreply.github.com>
2024-07-27 22:00:00 -0700

    Create numeric_parameter.hpp

---
+++ numeric_parameter.hpp
@@ -0,0 +1,32 @@
+#include <boost/numeric/conversion/cast.hpp>
+
+template <typename Target>
+class NumericParameter
+{
+public:
+    // This input is another Numeric Parameter
+    template<typename Source>NumericParameter(const NumericParameter<Source> &s)
+    {
+        value = NumericParameter<Target>(s.operator Source());
+    }
+
+    // The input in not a NumericParameter
+    template <typename Source>NumericParameter(Source s)
+    {
+        value =  boost::numeric_cast<Target>(s);
+    }
+
+    // This input is of type Target
+    NumericParameter(Target s)
+    {
+        value = s;
+    }
+
+    operator Target() const 
+    {
+        return value;
+    }
+
+private:
+    Target value;
+};
