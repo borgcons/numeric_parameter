b1f2d72375386420a46e3717a038ae5a15bc247a
Michael Borghardt <30298366+borgcons@users.noreply.github.com>
2024-08-18 17:14:50 -0700

    Using hpp file

--- test.cpp
+++ test.cpp
@@ -2,41 +2,6 @@
 #include <iostream> 
 #include <algorithm>
 
-template <typename Target>
-class NumericParameter
-{
-public:
-    // This input is another Numeric Parameter
-    template<typename Source>NumericParameter(const NumericParameter<Source> &s)
-    {
-        value = NumericParameter<Target>(s.operator Source());
-    }
-
-    // The input in not a NumericParameter
-    template <typename Source>NumericParameter(Source s)
-    {
-        value =  boost::numeric_cast<Target>(s);
-    }
-
-    // This input is of type Target
-    NumericParameter(Target s)
-    {
-        value = s;
-    }
-
-    operator Target() const 
-    {
-        return value;
-    }
-
-    template<typename Result> operator Result() const 
-    {
-        return boost::numeric_cast<Result>(value);
-    }
-private:
-    Target value;
-};
-
 int returnInt(NumericParameter<int> i)
 {
     return i;
